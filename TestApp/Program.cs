950b40e31232ec93bb3aed54d49f459153e99e7e
Paul Vizard <paulv@protolink.com>
2016-05-12 23:35:53 -0500

    Initial working decoding

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UINVComms;
 
 namespace TestApp
 {
@@ -10,6 +11,13 @@ namespace TestApp
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Opened port...");
+			SerialDevice serial = new SerialDevice("COM3", 9600);
+			serial.Open();
+
+
+			Console.ReadKey();
+			serial.Close();
 		}
 	}
 }
